9ff642a7798a82181a5cc53f10a68e6ddd733d59
unknown <bouras1920@gmail.com>
2022-06-08 18:10:49 +0100

    working with home page

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,11 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  generate: {
+    routes: [
+      '/blog/denali'
+    ]
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxtproject',
